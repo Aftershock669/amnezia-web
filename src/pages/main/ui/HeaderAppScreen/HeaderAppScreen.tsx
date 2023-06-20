99306100d65c7bc88bb41671eae9c9ea6064a90e
Aftershock669 <Aftershock669@gmail.com>
2023-06-20 22:29:11 +0400

    added logo glow + bg

--- src/pages/main/ui/HeaderAppScreen/HeaderAppScreen.tsx
+++ src/pages/main/ui/HeaderAppScreen/HeaderAppScreen.tsx
@@ -3,13 +3,25 @@ import styles from './HeaderAppScreen.module.scss';
 const HeaderAppScreen = () => {
   return (
     <div className={styles.root}>
+      <div className={styles.screenWrapper}>
+        <img
+          src="/app-screen-main.png"
+          className={styles.screenImg}
+          alt="Amnezia logo"
+        />
+      </div>
       <img
-        // height={25}
-        // width={94}
-        src="/app-screen-main.png"
-        className="logo"
+        src="/app-bg.svg"
+        className={styles.bgLightImg}
         alt="Amnezia logo"
       />
+      {/*<div className={styles.logoWrapper}>*/}
+        <img
+          src="/logo-bg.svg"
+          className={styles.bgLogoImg}
+          alt="Amnezia logo"
+        />
+      {/*</div>*/}
     </div>
   );
 };
