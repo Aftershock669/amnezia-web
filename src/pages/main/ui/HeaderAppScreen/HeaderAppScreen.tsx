0ea301b52bf2090e44e21f339e40a7a87509f952
Aftershock669 <Aftershock669@gmail.com>
2023-06-21 00:29:22 +0400

    alternative header

--- src/pages/main/ui/HeaderAppScreen/HeaderAppScreen.tsx
+++ src/pages/main/ui/HeaderAppScreen/HeaderAppScreen.tsx
@@ -1,27 +1,26 @@
+import classNames from 'classnames';
+import { useMediaQuery } from '@mantine/hooks';
 import styles from './HeaderAppScreen.module.scss';
 
 const HeaderAppScreen = () => {
+  const isDesktop = useMediaQuery('(min-width: 1024px)');
   return (
     <div className={styles.root}>
+      {isDesktop ? (
+        <div className={styles.outer}>
+          <div className={classNames(styles.ripple, styles.three)} />
+          <div className={classNames(styles.ripple, styles.two)} />
+          <div className={classNames(styles.ripple, styles.one)} />
+        </div>
+      ) : undefined}
       <div className={styles.screenWrapper}>
-        <img
-          src="/app-screen-main.png"
-          className={styles.screenImg}
-          alt="Amnezia logo"
-        />
+        <img src="/app-screen-main.png" className={styles.screenImg} alt="Amnezia logo" />
+        <div className={styles.glow} />
       </div>
-      <img
-        src="/app-bg.svg"
-        className={styles.bgLightImg}
-        alt="Amnezia logo"
-      />
-      {/*<div className={styles.logoWrapper}>*/}
-        <img
-          src="/logo-bg.svg"
-          className={styles.bgLogoImg}
-          alt="Amnezia logo"
-        />
-      {/*</div>*/}
+      {/* <img src="/app-bg.jpg" className={styles.bgLightImg} alt="Amnezia logo" /> */}
+      {/* <div className={styles.logoWrapper}> */}
+      {/* <img src="/logo-bg.svg" className={styles.bgLogoImg} alt="Amnezia logo" /> */}
+      {/* </div> */}
     </div>
   );
 };
